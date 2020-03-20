c0cddbccc75233e8697f247b406b636734c17018
Aleksei Korzhenkov <oscypek@ya.ru>
2020-03-20 10:42:09 +0300

    init

---
+++ pyyamb/tetra.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+import sys
+import os
+
+
+
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+
